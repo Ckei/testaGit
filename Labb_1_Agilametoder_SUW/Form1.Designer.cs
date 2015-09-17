1a30d3b63a907fdb0c74976c29cd96a55849887e
Ckei <daniel.heden@lernia.se>
2015-09-17 23:11:55 +0200

    change 1

---
+++ Labb_1_Agilametoder_SUW/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Labb_1_Agilametoder_SUW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contactListBox = new System.Windows.Forms.ListBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.mailTextBox = new System.Windows.Forms.TextBox();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.addContactButton = new System.Windows.Forms.Button();
+            this.printAllContacts = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contactListBox
+            // 
+            this.contactListBox.FormattingEnabled = true;
+            this.contactListBox.Location = new System.Drawing.Point(136, 7);
+            this.contactListBox.Name = "contactListBox";
+            this.contactListBox.Size = new System.Drawing.Size(650, 472);
+            this.contactListBox.TabIndex = 0;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(13, 13);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(117, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // mailTextBox
+            // 
+            this.mailTextBox.Location = new System.Drawing.Point(13, 40);
+            this.mailTextBox.Name = "mailTextBox";
+            this.mailTextBox.Size = new System.Drawing.Size(117, 20);
+            this.mailTextBox.TabIndex = 2;
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(13, 67);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(117, 20);
+            this.numberTextBox.TabIndex = 3;
+            // 
+            // addContactButton
+            // 
+            this.addContactButton.Location = new System.Drawing.Point(13, 94);
+            this.addContactButton.Name = "addContactButton";
+            this.addContactButton.Size = new System.Drawing.Size(117, 26);
+            this.addContactButton.TabIndex = 4;
+            this.addContactButton.Text = "Lägg till ny";
+            this.addContactButton.UseVisualStyleBackColor = true;
+            this.addContactButton.Click += new System.EventHandler(this.addContactButton_Click);
+            // 
+            // printAllContacts
+            // 
+            this.printAllContacts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.printAllContacts.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.printAllContacts.Location = new System.Drawing.Point(13, 170);
+            this.printAllContacts.Name = "printAllContacts";
+            this.printAllContacts.Size = new System.Drawing.Size(117, 62);
+            this.printAllContacts.TabIndex = 5;
+            this.printAllContacts.Text = "Uppdatera listan";
+            this.printAllContacts.UseVisualStyleBackColor = false;
+            this.printAllContacts.Click += new System.EventHandler(this.printAllContacts_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(13, 376);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(117, 93);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "Sök";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(13, 350);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(117, 20);
+            this.searchTextBox.TabIndex = 7;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(13, 126);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(117, 23);
+            this.deleteButton.TabIndex = 8;
+            this.deleteButton.Text = "Delete selected";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 481);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.printAllContacts);
+            this.Controls.Add(this.addContactButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.mailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.contactListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox contactListBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox mailTextBox;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button addContactButton;
+        private System.Windows.Forms.Button printAllContacts;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ Labb_1_Agilametoder_SUW/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labb_1_Agilametoder_SUW
+{
+    public partial class Form1 : Form
+    {
+        List<String> ContantList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            ContantList.Add("Namn     Mejl     Nummer");
+            ContantList.Add("Bengt     benga55@gmail.com     0701234567");
+            ContantList.Add("Peter     ptr@hotspot.com     0701234576");
+            ContantList.Add("Lina     lina_w@gmail.com     0701239654");
+            ContantList.Add("Ahmed     ahmed_ahmed@gmail.com     0707654321");
+            ContantList.Add("Angelica     angelangelica@gmail.com     0701212125");
+            ContantList.Add("Olof     olof_olofsson5@gmail.com     0702223336");
+            ContantList.Add("Clara     stensson_c@gmail.com     0730456789");
+            ContantList.Add("Stina     stinastinastinastina@hotmail.com     0700554412");
+            ContantList.Add("Bertil     beritochbertil@telia.com     0700000258");
+
+        }
+
+        //Adds a contact to our list
+        private void addContactButton_Click(object sender, EventArgs e)
+        {
+            ContantList.Add(nameTextBox.Text +"     "+ mailTextBox.Text + "     "+ numberTextBox.Text);
+            Clear();
+        }
+
+        //Updates our contactlist
+        private void printAllContacts_Click(object sender, EventArgs e)
+        {
+            Clear();
+            updateListItems();
+        }
+
+        //Updates the contactlist
+        private void updateListItems()
+        {
+            int i = 0; while (i < ContantList.Count)
+            {
+                contactListBox.Items.Add(ContantList.ElementAt(i));
+                i++;
+            }
+            //Sort my list
+            ContantList.Sort();
+
+        }
+
+        //Clears all text from the list and textboxes
+        private void Clear()
+        {
+            nameTextBox.Clear(); mailTextBox.Clear(); numberTextBox.Clear();
+            contactListBox.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string str = searchTextBox.Text;int i = 0;
+            while (i < ContantList.Count)
+            {
+                if (ContantList.ElementAt(i).Contains(str))
+                {
+                    MessageBox.Show("Hittade: " + ContantList.ElementAt(i));
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        //Delete selected item from the list
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            ContantList.RemoveAt(contactListBox.SelectedIndex);
+            Clear();
+            updateListItems();
+        }
+    }
+}
